4fe11007ab90bed32f3a24a62c9e6a50a8b59e5a
dvdrbn <david.rvb@gmail.com>
2020-11-24 10:30:20 +0200

    Added a stream printing function

--- transformer.py
+++ transformer.py
@@ -6,13 +6,45 @@ CODESET = 'utf-8'
 
 
 def extract_json(line):
-    jsonStart = jsonStart = line.index("{")
-    jsonEnd = len(line) - line[::-1].index("}")
-    return json.loads(line[jsonStart:jsonEnd])
+    """Return a json object if it exists in a string"""
+    jsonObj = None
+
+    if '{' in line and '}' in line:
+        jsonStart = line.find("{")
+        jsonEnd = len(line) - line[::-1].find("}")
+        jsonObj = json.loads(line[jsonStart:jsonEnd])
+    return jsonObj
+
+
+def stream_print(streamName, streamOut):
+    """
+    Print stream output returned from script execution. 
+    If JSON reponse was received, print it first for better clearance.
+    """
+
+    # if stream contained any output
+    if streamOut:
+        print('{} {}'.format(streamName, '-'*65))
+        jResp = extract_json(streamOut)
+
+        # if json response was part of the stream
+        if jResp:
+            print("Status: {}".format(jResp['status']))
+            print("Log:\n{}\n".format("\n".join(jResp['log'])))
+        else:
+            # service maybe offline if response is empty
+            print(
+                'No response received from server, it maybe offline. Please try again in a few minutes.')
+
+        # print full stream output
+        print('Full {} output:{}{}{}{}'.format(
+            streamName, '\n', '-'*19, '\n', streamOut))
+    else:
+        print('{} had no output'.format(streamName))
 
 
 # load json input for transformer
-with open('req.json') as json_data:
+with open('request.json') as json_data:
     jsonReq = json.load(json_data)
 
 try:
@@ -22,8 +54,8 @@ try:
 except subprocess.CalledProcessError as e:
     # print errors
     print("An error occured while running the script, please review the following:")
-    print("-- stdout: {}{}".format("\n", str(e.stdout, CODESET)))
-    print("-- stderr: {}{}".format("\n", str(e.stderr, CODESET)))
+    stream_print('stdout', str(e.stdout, CODESET))
+    stream_print('stderr', str(e.stderr, CODESET))
 else:
     # extract json response
     jsonResp = extract_json(str(output.stdout, CODESET))
